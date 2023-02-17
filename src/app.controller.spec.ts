b8dc3643ae36229835c6a59dcb9f05d8007324dd
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2023-02-17 13:50:32 +0100

    Initial commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+describe('AppController', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello from the Safe Gelato Relay Service!');
+  });
+});
