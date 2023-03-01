b0dece06ba35557559a7a73739e84080ae73ab60
schmanu <manu@safe.global>
2023-03-01 17:26:50 +0100

    fix: allow all origins in cors header

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning();
 
+  app.enableCors({
+    origin: '*',
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get('applicationPort');
 
