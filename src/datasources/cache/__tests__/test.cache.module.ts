e206d924e768a132186db33f5d7f6c552dd73d83
iamacook <aaron@safe.global>
2023-04-21 08:14:26 +0200

    fix: rename variable

--- src/datasources/cache/__tests__/test.cache.module.ts
+++ src/datasources/cache/__tests__/test.cache.module.ts
@@ -3,11 +3,11 @@ import { Global, Module } from '@nestjs/common';
 import { CacheService } from '../cache.service.interface';
 import { FakeCacheService } from './fake.cache.service';
 
-const mockCacheService = new FakeCacheService();
+const fakeCacheService = new FakeCacheService();
 
 @Global()
 @Module({
-  providers: [{ provide: CacheService, useValue: mockCacheService }],
+  providers: [{ provide: CacheService, useValue: fakeCacheService }],
   exports: [CacheService],
 })
 export class TestCacheModule {}
